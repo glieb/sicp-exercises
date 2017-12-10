9beff76def81726e339e721f532cdc49a978af98
glieb <glieb@users.noreply.github.com>
2017-12-10 15:33:31 -0500

    fix syntax; add interpreter-friendly versions

--- ch5/5.4.scm
+++ ch5/5.4.scm
@@ -18,7 +18,7 @@
     (save continue)
     (assign n (op -) (reg n) (const 1))
     (assign continue (label after-exp))
-    (goto exp-loop)
+    (goto (label exp-loop))
   after-exp
     (restore continue)
     (assign p (op *) (reg b) (reg p))
@@ -54,3 +54,39 @@
 ;;; equal to 'n', while the iterative solution simply allows the register 
 ;;; itself to function as a form of stack, without all the need for saving 
 ;;; labels and redirecting control.
+
+;;; Interpreter-friendly versions:
+
+(define expt-machine-recursive
+  (make-machine
+    '(b n p continue)
+    (list (list '= =) (list '- -) (list '* *))
+    '(begin
+        (assign continue (label exp-done))
+        (assign p (const 1))
+      exp-loop
+        (test (op =) (reg n) (const 0))
+        (branch (label after-exp))
+        (save continue)
+        (assign n (op -) (reg n) (const 1))
+        (assign continue (label after-exp))
+        (goto (label exp-loop))	
+      after-exp
+        (restore continue)
+        (assign p (op *) (reg b) (reg p))
+        (goto (reg continue))
+      exp-done)))
+
+(define expt-machine-iter
+  (make-machine
+    '(b n p)
+    (list (list '= =) (list '- -) (list '* *))
+    '(begin
+      (assign p (const 1))
+    exp-loop
+      (test (op =) (reg n) (const 0))
+      (branch (label exp-done))
+      (assign n (op -) (reg n) (const 1))
+      (assign p (op *) (reg b) (reg p))
+      (goto (label exp-loop))
+    exp-done)))
